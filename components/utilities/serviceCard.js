6b339ebf1dcb3353841c0f625becb291978a9235
Chidiadi1 <samrtchidiadi@gmail.com>
2025-05-21 20:32:37 -0700

    updating

--- components/utilities/serviceCard.js
+++ components/utilities/serviceCard.js
@@ -3,8 +3,7 @@ import clsx from "clsx";
 const ServiceCard = ( {icon: Icon, title, content, className}  ) => {
 
     return (
-        <div className={clsx("cursor-default p-4 md:p-10 rounded-3xl flex flex-col md:flex-row gap-4 md:gap-8 text-black-strong m-auto hover:rotate-6 transition-transform duration-300 hover:border-4 hover:border-white-normal from-white-normal to-blue-strong/5", className)}>
-            {/* <div className="inset-0 bg-[url('/grainy.jpeg')] bg-cover bg-center opacity-10 absolute -z-20 rounded-xl"></div> */}
+        <div className={clsx("cursor-default p-4 md:p-10 rounded-3xl flex flex-col md:flex-row gap-4 md:gap-8 text-black-strong m-auto hover:rotate-6 transition-transform duration-300 hover:border-4 hover:border-white-normal from-white-normal to-blue-strong/10", className)}>
             <div>
                 <Icon className="size-14 md:size-30 text-blue-strong"/>
             </div>
